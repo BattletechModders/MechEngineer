83ac483ecf4b8deea859e674cda82541ef0af7b1
CrusherBob <39445749+CrusherBob@users.noreply.github.com>
2018-05-20 10:33:06 +0200

    crusher bob engine overhaul initial commit

--- README.md
+++ README.md
@@ -16,7 +16,6 @@ or
 
 * added std engine type
 * added xl engine type and side torso xl engine parts
-* removed global 10 heatsinks, each engine comes with TT free heatsink count
 * calculate walk and run speeds based on engine rating and mech tonnage
 * all mechs have 10% their max tonnage as initial tonnage
 * mechlab enforces to use an engine on a mech, xl engine requires the side torso parts
@@ -26,14 +25,8 @@ or
 * hide incompatible engines for mech
 * engine tonnage determins install tech cost
 * crit engine parts reduce speed, destroyed engine parts destroy mech
-* max jump jet is calculated linear to the speed of the mech up to 8 JJ at max sprint speed
+* jump jets should now be limited to the integer distance of the mech's TT walk speed
 
-TBD:
-* add stock engines to each mech (first add advanced array manipulation feature to modtek)
-* sort out what engine ratings to add to the game (need stock engines for mechs, get rid of xl again?)
-* figure out economy of engines (tech costs factor)
-* availability (shops and salvage settings)
-* more?
 
 ## Download
 
--- source/Control.cs
+++ source/Control.cs
@@ -21,7 +21,58 @@ namespace MechEngineMod
         public float? MinSprintFactor = null;
         public int FallbackHeatSinkCapacity = 30;
         public float SpeedMultiplierPerDamagedEnginePart = 0.7f;
+		
+		//start Crusher Bob Additions
+		/* 
+		set to false to use TT walk values
+		using the default game values, slow mechs move a bit faster, and fast mechs move a bit slower
+		Examples if set to true:
+			Walk 2  70 / 125
+			Walk 3  95 / 165
+			Walk 4 120 / 200
+			Walk 5 140 / 240
+			Walk 6 165 / 275
+			Walk 7 190 / 315
+			Walk 8 210 / 350
+		*/
+		public bool UseGameWalkValues = true;
+		
+		// set to false to only allow engines that produce integer walk values
+		public bool AllowNonIntWalkValues = true;
+		
+		// this setting controls if the allowed number of jump jets is rounded up or down
+		// example: if false, TT walk speed of 2.1 allows 2 jump jets, if true, it allows 3 jump jets
+		public bool JJRoundUp = false;
+		
+		// this controls the maximum (global) allowed jump jets.
+		// currently set to 8 in case the game can't handle anymore
+		public int const_MaxNumberOfJJ = 8;
+		
+		/*
+		these numbers determine how fast and slow mechs are allowed to go
+		They are given in tabletop walk numbers.
+		Examples: 
+			urbanmech walk = 2
+			griffin walk = 5
+			spider walk = 8
+		*/
+		public int const_MinTTWalk = 2;
+		public int const_MaxTTWalk = 8;
+		
+		/*
+		not sure why you would want to change these, but they are set here
+		they are the multiples that translate TT movement values to game movement values
+		Example:
+			A griffin that walks 5 would walk 5 * 30 = 150 and sprint 5 * 50 = 250
+		NOTE: if you have the UseGameWalkValues set, the exact values are then changed based on a linear equasion
+		*/
+		public float const_TTWalkMultiplier = 30f;
+		public float const_TTSprintMultiplier = 50f;
+		//end Crusher Bob Additions
+		
     }
+	
+
 
     internal class EngineCalculator
     {
@@ -31,13 +82,22 @@ namespace MechEngineMod
         // CDA-2A 350d 40t 320r // 44f
         // SHD-2H 240d 55t 275r // 48f
         // AS7-D 165d 100t 300r // 55f
+		
+		//Crusher Bob: this value depreciated, use const_TTWalkMultiplier and const_TTSprintMultiplier instead
+		/*
         internal static float factorwalkMPtoSprintDistance = 50;
-
+		*/
+		//Crusher Bob: end removed original code
+		
+		//Crusher Bob: Removed original code
+		//use const_MinTTWalk and const_MaxTTWalk instead; sidesteps problems with curved vs non curved walk/sprint values
+		/*
         internal static float MaxSprintFactor
         {
             get
             {
                 //"MaxSprintFactor" : 350.0
+				//Crusher Bob comment: is set in MechStatisticsConstants.json
                 return Control.settings.MaxSprintFactor ?? UnityGameInstance.BattleTechGame.MechStatisticsConstants.MaxSprintFactor;
             }
         }
@@ -47,9 +107,29 @@ namespace MechEngineMod
             get
             {
                 //"MinSprintFactor" : 125.0
+				//Crusher Bob comment: is set in MechStatisticsConstants.json
                 return Control.settings.MinSprintFactor ?? UnityGameInstance.BattleTechGame.MechStatisticsConstants.MinSprintFactor;
             }
         }
+		*/
+		//Crusher Bob: end removed original code
+		
+		//start Crusher Bob Additions
+		internal static float func_Roundby5(float value)
+		/*
+			examples:
+				func_Roundby5(2.49) = 0
+				func_Roundby5(2.5) = 5
+				func_Roundby5(7.49) = 5
+				func_Roundby5(7.5) = 10
+		*/
+		{
+			if (value % 5f < 2.5)
+				return (value - (value % 5f));
+			else
+				return (value - (value % 5f) + 5f);
+		}
+		//end Crusher Bob Additions
 
         internal bool CalcAvailability(Engine engine, float tonnage)
         {
@@ -60,15 +140,35 @@ namespace MechEngineMod
             //    return false;
             //}
 
+			//Crusher Bob: removed original code
+			/*
             var sprintDistance = DistanceFormula(engine.Rating, tonnage);
+			*/
+			//Crusher Bob: end removed original code
+			
+			//start Crusher Bob Additions
+			float TTWalkDistance = engine.Rating / tonnage;
+			//end Crusher Bob Additions
 
             Control.mod.Logger.LogDebug("CalcAvailability" +
                                         " rating=" + engine.Rating +
                                         " tonnage=" + tonnage +
+										//Crusher Bob: removed original code
+										/*
                                         " sprintDistance=" + sprintDistance +
                                         " MaxSprintFactor=" + MaxSprintFactor +
                                         " MinSprintFactor=" + MinSprintFactor);
+										*/
+										//Crusher Bob: end removed original code
+										//start Crusher Bob Additions
+                                        " TTWalkDistance=" + TTWalkDistance +
+										" Min Walk =" + Control.settings.const_MinTTWalk +
+										" Max Walk =" + Control.settings.const_MaxTTWalk);
+										//end Crusher Bob Additions
 
+			//Crusher Bob: Removed original code
+			//checks now based on const_MinTTWalk and const_MaxTTWalk instead
+			/*
             // max rating due to sprint max
             if (sprintDistance > MaxSprintFactor)
             {
@@ -80,44 +180,154 @@ namespace MechEngineMod
             {
                 return false;
             }
+			*/
+			//Crusher Bob: end removed original code
+			
+			//start Crusher Bob Additions
+
+            // check if over max walk distance
+            if (TTWalkDistance > Control.settings.const_MaxTTWalk)
+            {
+                return false;
+            }
+
+            // check if below min walk distance
+            if (TTWalkDistance < Control.settings.const_MinTTWalk)
+            {
+                return false;
+            }
+			
+			//check if non integer TT walk values are allowed
+			if (Control.settings.AllowNonIntWalkValues == false)
+			{
+				//if not, check that walk value is an integer
+				if ((TTWalkDistance % 1f) != 0)
+					return false;
+			}
+			//end Crusher Bob Additions
 
             return true;
         }
-
+		//Crusher Bob: removed original code
+		/*
         internal void CalcSpeeds(Engine engine, float tonnage, out float walkSpeed, out float runSpeed)
+		*/
+		//Crusher Bob: end removed original code
+			
+		//start Crusher Bob Additions
+		internal void CalcSpeeds(Engine engine, float tonnage, out float walkSpeed, out float runSpeed, out float TTWalkSpeed)
+		//end Crusher Bob Additions
         {
-            runSpeed = DistanceFormula(engine.Rating, tonnage);
+
+			//Crusher Bob: removed original code
+			/*
+			runSpeed = DistanceFormula(engine.Rating, tonnage);
             walkSpeed = runSpeed * 3 / 5;
+			*/
+			//Crusher Bob: end removed original code
+			
+			//start Crusher Bob Additions
+			TTWalkSpeed = engine.Rating / tonnage;
+			walkSpeed = Calc_WalkDistance(TTWalkSpeed);
+			runSpeed = Calc_SprintDistance(TTWalkSpeed);
+			//end Crusher Bob Additions
+
+			
             Control.mod.Logger.LogDebug("CalcSpeeds" +
                                         " rating=" + engine.Rating +
                                         " tonnage=" + tonnage +
                                         " walkSpeed=" + walkSpeed +
-                                        " runSpeed=" + runSpeed);
+                                        " runSpeed=" + runSpeed 
+										//start Crusher Bob Additions
+										+
+										" TTWalkSpeed=" + TTWalkSpeed)
+										//end Crusher Bob Additions
+										;
         }
 
         internal int CalcInstallTechCost(Engine engine)
         {
             return Mathf.CeilToInt(Control.settings.TechCostPerEngineTon * engine.Def.Tonnage);
         }
-        
+		
         internal int CalcJumpJetCount(Engine engine, float tonnage)
         {
-            var runSpeed = DistanceFormula(engine.Rating, tonnage);
+		
+			//Crusher Bob: Removed original code
+			//Now just base the allowed number of jump jets on the calculated TT walk speed
+            /*
+			var runSpeed = DistanceFormula(engine.Rating, tonnage);
             var percentOfMaxSpeed = (runSpeed - MinSprintFactor) / (MaxSprintFactor - MinSprintFactor);
 
             var maxSpeedByTTRules = 8;
-            return Mathf.CeilToInt(percentOfMaxSpeed * maxSpeedByTTRules);
+			
+			return Mathf.CeilToInt(percentOfMaxSpeed * maxSpeedByTTRules);
+			*/
+			//Crusher Bob: end removed original code
+			
+			//start Crusher Bob Additions
+			float TTWalkSpeed = engine.Rating / tonnage;
+			float AllowedJJs = Math.Min(TTWalkSpeed, Control.settings.const_MaxNumberOfJJ);
+			
+			if (Control.settings.JJRoundUp == true)
+			//end Crusher Bob Additions
+				return Mathf.CeilToInt(AllowedJJs);
+			//start Crusher Bob Additions
+			else
+				return Mathf.FloorToInt(AllowedJJs);
+			//end Crusher Bob Additions
         }
-
+		
+		//Crusher Bob: this function depreciated, use Calc_WalkDistance and Calc_SprintDistance instead
+		/*
         private static float DistanceFormula(int rating, float tonnage)
         {
             return rating / tonnage * factorwalkMPtoSprintDistance;
         }
+		*/
+		//Crusher Bob: end removed original code
+
+		//start Crusher Bob Additions
+        private static float Calc_WalkDistance(float TTWalkSpeed)
+		// numbers the result of the best fit line for the game movement
+        {
+			float WalkSpeedFixed = 26.05f;
+			float WalkSpeedMult = 23.14f;
+		
+			if (Control.settings.UseGameWalkValues == true)
+				return func_Roundby5(WalkSpeedFixed + TTWalkSpeed * WalkSpeedMult);
+			else
+				return func_Roundby5(TTWalkSpeed * Control.settings.const_TTWalkMultiplier);
+			
+		}
+		
+        private static float Calc_SprintDistance(float TTWalkSpeed)
+		// numbers the result of the best fit line for the game movement
+        {
+			float RunSpeedFixed = 52.43f;
+			float RunSpeedMult = 37.29f;
+			
+			if (Control.settings.UseGameWalkValues == true)
+				return func_Roundby5(RunSpeedFixed + TTWalkSpeed * RunSpeedMult);
+			else
+				return func_Roundby5(TTWalkSpeed * Control.settings.const_TTSprintMultiplier);
+		}
+		//end Crusher Bob Additions
+		
     }
 
     internal enum EngineType
     {
-        Std, XL
+		//Crusher Bob: removed original code
+		/*
+		Std, XL
+		*/
+		//Crusher Bob: end removed original code
+
+		//start Crusher Bob Additions
+		//making provisions for engines with single or double heat sinks
+		Std_shs, Std_dhs, XL_shs, XL_dhs
+		//end Crusher Bob Additions
     }
 
     internal class Engine
@@ -208,6 +418,8 @@ namespace MechEngineMod
             }
         }
 
+		//Crusher Bob: this function depreciated. Use chassis def files instead, to allow for endo steel, etc
+		/*
         // set initial weight of mechs to 0.1 times the tonnage
         [HarmonyPatch(typeof(ChassisDef), "FromJSON")]
         public static class ChassisDefPatch
@@ -228,9 +440,11 @@ namespace MechEngineMod
                 }
             }
         }
+		*/
+		//Crusher Bob: end removed original code
 
         // invalidate mech loadouts that don't have an engine
-        // invalidate mech loadouts that have more jump jets than the engine supports
+		// invalidate mech loadouts that have more jump jets than the engine supports
         [HarmonyPatch(typeof(MechValidationRules), "ValidateMechPosessesWeapons")]
         public static class MechValidationRulesPatch
         {
@@ -246,17 +460,27 @@ namespace MechEngineMod
                     {
                         errorMessages[MechValidationType.InvalidInventorySlots].Add("MISSING ENGINE: This Mech must mount a functional Fusion Engine");
                     }
-                    else if (mainEngine.Type == EngineType.XL && engineRefs.Count(x => x.DamageLevel == ComponentDamageLevel.Functional || x.DamageLevel == ComponentDamageLevel.NonFunctional) != 3)
+					//Crusher Bob: removed original code
+					/*
+                    else if (mainEngine.Type == EngineType.XL && engineRefs.Count(x => x.DamageLevel ==
+					*/
+					//Crusher Bob: end removed original code
+					
+					//start Crusher Bob additions
+					else if ((mainEngine.Type == EngineType.XL_shs || mainEngine.Type == EngineType.XL_dhs )&& engineRefs.Count(x => x.DamageLevel ==
+					//end Crusher Bob additions
+					ComponentDamageLevel.Functional || x.DamageLevel == ComponentDamageLevel.NonFunctional) != 3)
                     {
                         errorMessages[MechValidationType.InvalidInventorySlots].Add("INCOMPLETE ENGINE: An XL Engine requires left and right torso components");
                     }
-
+					
                     var currentCount = mechDef.Inventory.Count(c => c.ComponentDefType == ComponentType.JumpJet);
                     var maxCount = calc.CalcJumpJetCount(mainEngine, mechDef.Chassis.Tonnage); ;
                     if (currentCount > maxCount)
                     {
                         errorMessages[MechValidationType.InvalidJumpjets].Add(string.Format("JUMPJETS: This 'Mech mounts too many jumpjets ({0} out of {1})", currentCount, maxCount));
                     }
+					
                 }
                 catch (Exception e)
                 {
@@ -264,7 +488,7 @@ namespace MechEngineMod
                 }
             }
         }
-
+        
         // only allow one engine part per specific location
         [HarmonyPatch(typeof(MechLabLocationWidget), "ValidateAdd", new[] { typeof(MechComponentDef) })]
         public static class MechLabLocationWidgetEnginePatch
@@ -370,15 +594,31 @@ namespace MechEngineMod
                     //since we don't want to fiddle around too much, ignore current tonnage
                     //var currentTonnage = 0f;
                     //MechStatisticsRules.CalculateTonnage(mechDef, ref currentTonnage, ref maxTonnage);
-                    float walkSpeed, runSpeed;
-                    calc.CalcSpeeds(engine, maxTonnage, out walkSpeed, out runSpeed);
+                    float walkSpeed, runSpeed, TTwalkSpeed;
+                    calc.CalcSpeeds(engine, maxTonnage, out walkSpeed, out runSpeed, out TTwalkSpeed);
                     var maxSprintDistance = runSpeed;
 
+					//Crusher Bob: removed original code
+					/*
                     currentValue = Mathf.Floor(
                         (maxSprintDistance - UnityGameInstance.BattleTechGame.MechStatisticsConstants.MinSprintFactor)
                         / (UnityGameInstance.BattleTechGame.MechStatisticsConstants.MaxSprintFactor - UnityGameInstance.BattleTechGame.MechStatisticsConstants.MinSprintFactor)
                         * 10f
                     );
+					*/
+					//Crusher Bob: end removed original code
+
+					//start Crusher Bob additions
+					//used much more familiar TT walk speed values
+					//also, user doesn't have to change min/max sprint values depending on if they are using curved movement or not
+					//divided by 9 instead of 10 to make scale more reactive at the bottom.
+                    currentValue = Mathf.Floor(
+                        (TTwalkSpeed - Control.settings.const_MinTTWalk)
+                        / (Control.settings.const_MaxTTWalk - Control.settings.const_MinTTWalk)
+                        * 9f +1
+                    );
+					//end Crusher Bob additions
+					
                     currentValue = Mathf.Max(currentValue, 1f);
                     maxValue = 10f;
                     return false;
@@ -462,8 +702,8 @@ namespace MechEngineMod
                     
                     var tonnage = __instance.tonnage;
 
-                    float walkSpeed, runSpeed;
-                    calc.CalcSpeeds(engine, tonnage, out walkSpeed, out runSpeed);
+                    float walkSpeed, runSpeed, TTWalkSpeed;
+                    calc.CalcSpeeds(engine, tonnage, out walkSpeed, out runSpeed, out TTWalkSpeed);
 
                     __instance.StatCollection.GetStatistic("WalkSpeed").SetValue(walkSpeed);
                     __instance.StatCollection.GetStatistic("RunSpeed").SetValue(runSpeed);
