729279c4130f30e51490b8074b3a03b6b709270d
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-06-20 02:15:11 +0200

    added to stat tooltips and switched from prefix code clones to transpiler

--- source/Patches/MechStatisticsRulesCalculateHeatEfficiencyStatPatch.cs
+++ source/Patches/MechStatisticsRulesCalculateHeatEfficiencyStatPatch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BattleTech;
 using Harmony;
@@ -6,148 +7,69 @@ using UnityEngine;
 
 namespace MechEngineMod
 {
-    // caluclate heat efficency and support engine heat dissipation
+    [HarmonyPatch(typeof(StatTooltipData), "SetHeatData")]
+    public static class StatTooltipDataSetHeatDataPatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.MethodReplacer(
+                AccessTools.Method(typeof(HeatSinkDef), "get_DissipationCapacity"),
+                AccessTools.Method(typeof(StatTooltipDataSetHeatDataPatch), "DissipationCapacity")
+            );
+        }
+
+        private static MechDef mechDef;
+
+        public static void Prefix(MechDef def)
+        {
+            mechDef = def;
+        }
+
+        public static void Postfix()
+        {
+            mechDef = null;
+        }
+
+        public static float DissipationCapacity(this HeatSinkDef @this)
+        {
+            if (@this.IsMainEngine())
+            {
+                return EngineHeat.GetEngineHeatDissipation(mechDef.Inventory);
+            }
+            return @this.DissipationCapacity;
+        }
+    }
+
     [HarmonyPatch(typeof(MechStatisticsRules), "CalculateHeatEfficiencyStat")]
     public static class MechStatisticsRulesCalculateHeatEfficiencyStatPatch
     {
-
-        public static bool Prefix(MechDef mechDef, ref float currentValue, ref float maxValue)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            try
-            {
-                return CalculateHeatEfficiencyStat(mechDef, ref currentValue, ref maxValue);
-            }
-            catch (Exception e)
-            {
-                Control.mod.Logger.LogError(e);
-                return true;
-            }
+            return instructions.MethodReplacer(
+                AccessTools.Method(typeof(HeatSinkDef), "get_DissipationCapacity"),
+                AccessTools.Method(typeof(MechStatisticsRulesCalculateHeatEfficiencyStatPatch), "DissipationCapacity")
+            );
         }
 
-        private static float AdditionalHeatSinkDissipation(MechDef mechDef)
+        private static MechDef def;
+
+        public static void Prefix(MechDef mechDef)
         {
-            var engineHeatSinkDissipation = EngineHeat.GetEngineHeatDissipation(mechDef.Inventory);
-            //Control.mod.Logger.LogDebug("engineHeatSinkDissipation=" + engineHeatSinkDissipation);
-            return engineHeatSinkDissipation;
+            def = mechDef;
         }
 
-        private static bool CalculateHeatEfficiencyStat(MechDef mechDef, ref float currentValue, ref float maxValue)
+        public static void Postfix()
         {
-            var totalHeatSinkDissipation = Control.Combat.Heat.InternalHeatSinkCount * Control.Combat.Heat.DefaultHeatSinkDissipationCapacity;
-            var heatGenerationWeapons = 0f;
-            var numberOfJumpJets = 0;
+            def = null;
+        }
 
-            foreach (var mechComponentRef in mechDef.Inventory)
+        public static float DissipationCapacity(this HeatSinkDef @this)
+        {
+            if (@this.IsMainEngine())
             {
-                if (mechComponentRef.Def == null)
-                {
-                    mechComponentRef.RefreshComponentDef();
-                }
-                if (mechComponentRef.Def is WeaponDef)
-                {
-                    var weaponDef = (WeaponDef)mechComponentRef.Def;
-                    heatGenerationWeapons += weaponDef.HeatGenerated;
-                }
-                else if (mechComponentRef.ComponentDefType == ComponentType.JumpJet)
-                {
-                    if (mechComponentRef.DamageLevel < ComponentDamageLevel.NonFunctional)
-                    {
-                        numberOfJumpJets++;
-                    }
-                }
-                else if (mechComponentRef.Def is HeatSinkDef)
-                {
-                    var heatSinkDef = (HeatSinkDef)mechComponentRef.Def;
-                    totalHeatSinkDissipation += heatSinkDef.DissipationCapacity;
-                }
+                return EngineHeat.GetEngineHeatDissipation(def.Inventory);
             }
-            
-            totalHeatSinkDissipation += AdditionalHeatSinkDissipation(mechDef);
-
-            //Control.mod.Logger.LogDebug("heatGenerationWeapons=" + heatGenerationWeapons);
-            //Control.mod.Logger.LogDebug("totalHeatSinkDissipation=" + totalHeatSinkDissipation);
-
-            var maxHeat = Control.Combat.Heat.MaxHeat;
-            {
-                var stats = new StatCollection();
-                var maxHeatStatistic = stats.AddStatistic("MaxHeat", maxHeat);
-                var heatGeneratedStatistic = stats.AddStatistic("HeatGenerated", heatGenerationWeapons);
-
-                foreach (var mechComponentRef in mechDef.Inventory)
-                {
-                    if (mechComponentRef.Def == null || mechComponentRef.Def.statusEffects == null)
-                    {
-                        continue;
-                    }
-
-                    var statusEffects = mechComponentRef.Def.statusEffects;
-                    foreach (var effect in statusEffects)
-                    {
-                        switch (effect.statisticData.statName)
-                        {
-                            case "MaxHeat":
-                                stats.PerformOperation(maxHeatStatistic, effect.statisticData);
-                                break;
-                            case "HeatGenerated":
-                                if (effect.statisticData.targetCollection == StatisticEffectData.TargetCollection.Weapon)
-                                {
-                                    stats.PerformOperation(heatGeneratedStatistic, effect.statisticData);
-                                }
-                                break;
-                        }
-                    }
-                }
-
-                maxHeat = maxHeatStatistic.CurrentValue.Value<int>();
-                heatGenerationWeapons = heatGeneratedStatistic.CurrentValue.Value<float>();
-            }
-
-            //Control.mod.Logger.LogDebug("maxHeat=" + maxHeat);
-            //Control.mod.Logger.LogDebug("heatGenerationWeapons=" + heatGenerationWeapons);
-
-            if (numberOfJumpJets >= Control.Combat.MoveConstants.MoveTable.Length)
-            {
-                numberOfJumpJets = Control.Combat.MoveConstants.MoveTable.Length - 1;
-            }
-
-            var heatGenerationJumpJets = 0f;
-            var jumpHeatDivisor = 3;
-            if (numberOfJumpJets > 0)
-            {
-                heatGenerationJumpJets += numberOfJumpJets * Control.Combat.Heat.JumpHeatUnitSize / jumpHeatDivisor;
-            }
-            else
-            {
-                heatGenerationJumpJets = 0f;
-            }
-
-            //Control.mod.Logger.LogDebug("heatGenerationJumpJets=" + heatGenerationJumpJets);
-
-            totalHeatSinkDissipation *= Control.Combat.Heat.GlobalHeatSinkMultiplier;
-            var totalHeatGeneration = (heatGenerationWeapons + heatGenerationJumpJets) * Control.Combat.Heat.GlobalHeatIncreaseMultiplier;
-
-            //Control.mod.Logger.LogDebug("totalHeatGeneration=" + totalHeatGeneration);
-
-            // rounding steps for heatSinkDissipation
-            var heatDissipationPercent = Mathf.Min(totalHeatSinkDissipation / totalHeatGeneration * 100f, UnityGameInstance.BattleTechGame.MechStatisticsConstants.MaxHeatEfficiency);
-            heatDissipationPercent = Mathf.Max(heatDissipationPercent, UnityGameInstance.BattleTechGame.MechStatisticsConstants.MinHeatEfficiency);
-
-            //Control.mod.Logger.LogDebug("heatDissipationPercent=" + heatDissipationPercent);
-
-            totalHeatSinkDissipation = totalHeatGeneration * (heatDissipationPercent / 100f);
-
-            //Control.mod.Logger.LogDebug("totalHeatSinkDissipation=" + totalHeatSinkDissipation);
-
-            var heatLeftOver = totalHeatGeneration - totalHeatSinkDissipation;
-            var unusedHeatCapacity = maxHeat - heatLeftOver;
-
-            //Control.mod.Logger.LogDebug("heatLeftOver=" + heatLeftOver);
-            //Control.mod.Logger.LogDebug("unusedHeatCapacity=" + unusedHeatCapacity);
-
-            currentValue = Mathf.Round((unusedHeatCapacity / maxHeat) * 10f);
-            currentValue = Mathf.Max(Mathf.Min(currentValue, 10f), 1f);
-            maxValue = 10f;
-            return false;
+            return @this.DissipationCapacity;
         }
     }
 }
--- source/Patches/MechStatisticsRulesCalculateMovementStatPatch.cs
+++ source/Patches/MechStatisticsRulesCalculateMovementStatPatch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BattleTech;
 using Harmony;
@@ -6,21 +7,93 @@ using UnityEngine;
 
 namespace MechEngineMod
 {
+    [HarmonyPatch(typeof(StatTooltipData), "SetMovementData")]
+    public static class StatTooltipDataSetMovementDataPatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions
+                .MethodReplacer(
+                    AccessTools.Method(typeof(MovementCapabilitiesDef), "get_MaxWalkDistance"),
+                    AccessTools.Method(typeof(StatTooltipDataSetMovementDataPatch), "MaxWalkDistance")
+                    )
+                .MethodReplacer(
+                    AccessTools.Method(typeof(MovementCapabilitiesDef), "get_MaxSprintDistance"),
+                    AccessTools.Method(typeof(StatTooltipDataSetMovementDataPatch), "MaxSprintDistance")
+                    );
+        }
+
+        private static MechDef mechDef;
+        private static float TTWalkSpeed;
+
+        public static void Prefix(MechDef def)
+        {
+            mechDef = def;
+        }
+
+        public static void Postfix(StatTooltipData __instance)
+        {
+            mechDef = null;
+            __instance.dataList.Add("TT Walk MP", string.Format("{0}", TTWalkSpeed));
+        }
+
+        public static float MaxWalkDistance(this MovementCapabilitiesDef @this)
+        {
+            float walkSpeed = 0, runSpeed = 0;
+            Engine.CalculateMovementStat(mechDef, ref walkSpeed, ref runSpeed, ref TTWalkSpeed);
+            return walkSpeed;
+        }
+
+        public static float MaxSprintDistance(this MovementCapabilitiesDef @this)
+        {
+            float walkSpeed = 0, runSpeed = 0, TTWalkSpeed = 0;
+            Engine.CalculateMovementStat(mechDef, ref walkSpeed, ref runSpeed, ref TTWalkSpeed);
+            return runSpeed;
+        }
+    }
+
     [HarmonyPatch(typeof(MechStatisticsRules), "CalculateMovementStat")]
     public static class MechStatisticsRulesCalculateMovementStatPatch
     {
-        // make the mech movement summary stat be calculated using the engine
-        public static bool Prefix(MechDef mechDef, ref float currentValue, ref float maxValue)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            try
-            {
-                return Engine.CalculateMovementStat(mechDef, ref currentValue, ref maxValue);
-            }
-            catch (Exception e)
-            {
-                Control.mod.Logger.LogError(e);
-                return true;
-            }
+            return instructions
+                .MethodReplacer(
+                    AccessTools.Method(typeof(MovementCapabilitiesDef), "get_MaxSprintDistance"),
+                    AccessTools.Method(typeof(MechStatisticsRulesCalculateMovementStatPatch), "MaxSprintDistance")
+                    )
+                .MethodReplacer(
+                    AccessTools.Method(typeof(MechDef), "get_Inventory"),
+                    AccessTools.Method(typeof(MechStatisticsRulesCalculateMovementStatPatch), "Inventory")
+                    );
+        }
+
+        private static MechDef def;
+
+        public static void Prefix(MechDef mechDef)
+        {
+            def = mechDef;
+        }
+
+        public static void Postfix()
+        {
+            def = null;
+        }
+
+        public static float MaxSprintDistance(this MovementCapabilitiesDef @this)
+        {
+            float walkSpeed = 0, runSpeed = 0, TTWalkSpeed = 0;
+            Engine.CalculateMovementStat(def, ref walkSpeed, ref runSpeed, ref TTWalkSpeed);
+            return runSpeed;
+        }
+        
+        // disable jump jet calculations
+        private static readonly MechComponentRef[] Empty = { };
+        public static MechComponentRef[] Inventory(this MechDef @this)
+        {
+            return Empty;
         }
     }
 }
+
+
