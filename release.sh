9909917277f05a41335a58f96c7f44ced05f1541
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-07-06 23:13:02 +0200

    updated build script

--- release.sh
+++ release.sh
@@ -7,7 +7,8 @@ cd ..
 SEVENZIP="/c/Program Files/7-Zip/7z"
 
 INCLUDES="-ir!MechEngineer"
-INCLUDES_ALL="$INCLUDES -ir!CBTHeat -ir!CBTMovement -ir!CustomComponent -ir!DynModLib -ir!HardpointFixMod -ir!pansar -ir!SkipTutorial -ir!SpeedMod ModTek.dll"
+# -ir!CustomComponent 
+INCLUDES_ALL="$INCLUDES -ir!CBTHeat -ir!CBTMovement -ir!DynModLib -ir!HardpointFixMod -ir!pansar -ir!SkipTutorial -ir!SpeedMod ModTek.dll"
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.user -xr!bin -xr!obj -xr!.vs -xr!.git -xr!_ignored -xr!*.zip"
 
 "$SEVENZIP" a -tzip -mx0 MechEngineer/MechEngineer.zip $EXCLUDES $INCLUDES
