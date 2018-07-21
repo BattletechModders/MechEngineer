875e6fa982489bace4aea272209b35d3a58dc49b
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-07-21 16:28:36 +0200

    implemented default for work order transitions

--- source/Features/WorkOrderCosts/WorkOrderCosts.cs
+++ source/Features/WorkOrderCosts/WorkOrderCosts.cs
@@ -1,10 +1,12 @@
-﻿using CustomComponents;
+﻿using System.Collections.Generic;
+using CustomComponents;
 
 namespace MechEngineer
 {
     [CustomComponent("WorkOrderCosts")]
-    public class WorkOrderCosts : SimpleCustomComponent
+    public class WorkOrderCosts : SimpleCustomComponent, IAfterLoad
     {
+        public Costs Default { get; set; }
         public Costs Install { get; set; }
         public Costs Repair { get; set; }
         public Costs RepairDestroyed { get; set; }
@@ -15,6 +17,15 @@ namespace MechEngineer
         {
             public string TechCost { get; set; }
             public string CBillCost { get; set; }
+        }
+
+        public void OnLoaded(Dictionary<string, object> values)
+        {
+            Install = Install ?? Default;
+            Repair = Repair ?? Default;
+            RepairDestroyed = RepairDestroyed ?? Default;
+            Remove = Remove ?? Default;
+            RemoveDestroyed = RemoveDestroyed ?? Default;
         }
     }
 }
