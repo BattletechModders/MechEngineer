43827b05e56083e940ea2e7e39595dbaa130f1b7
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-08-13 00:02:52 +0200

    added movement in hex unit

--- source/Engine/Patches/StatTooltipData_SetMovementData_Patch.cs
+++ source/Engine/Patches/StatTooltipData_SetMovementData_Patch.cs
@@ -2,91 +2,39 @@
 using System.Collections.Generic;
 using BattleTech;
 using Harmony;
+using UnityEngine;
 
 namespace MechEngineer
 {
     [HarmonyPatch(typeof(StatTooltipData), "SetMovementData")]
     public static class StatTooltipData_SetMovementData_Patch
     {
-        private static MechDef mechDef;
-        private static EngineMovement movement;
 
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            return instructions
-                .MethodReplacer(
-                    AccessTools.Method(typeof(MovementCapabilitiesDef), "get_MaxWalkDistance"),
-                    AccessTools.Method(typeof(StatTooltipData_SetMovementData_Patch), "OverrideMaxWalkDistance")
-                )
-                .MethodReplacer(
-                    AccessTools.Method(typeof(MovementCapabilitiesDef), "get_MaxSprintDistance"),
-                    AccessTools.Method(typeof(StatTooltipData_SetMovementData_Patch), "OverrideMaxSprintDistance")
-                );
-        }
-
-        public static void Prefix(MechDef def)
+        public static void Postfix(StatTooltipData __instance, MechDef def)
         {
             try
             {
-                mechDef = def;
-            }
-            catch (Exception e)
-            {
-                Control.mod.Logger.LogError(e);
-            }
-        }
-
-        public static void Postfix(StatTooltipData __instance)
-        {
-            try
-            {
-                var addition = movement?.MovementPoint.ToString();
-                if (addition != null)
+                var movement = def.GetEngineMovement();
+                if (movement == null)
                 {
-                    __instance.dataList.Add("TT Walk MP", addition);
+                    return;
                 }
+
+                var tooltipData = __instance;
+
+                tooltipData.dataList.Remove("Max Move");
+                tooltipData.dataList.Remove("Max Sprint");
+                
+                var combat = CombatGameConstants.CreateFromSaved(UnityGameInstance.BattleTechGame);
+                var hexWidth = combat.MoveConstants.ExperimentalGridDistance;
+                __instance.dataList.Add("Max Move", $"{movement.WalkSpeed}m / {Mathf.Round(movement.WalkSpeed / hexWidth)} hex");
+                __instance.dataList.Add("Max Sprint", $"{movement.RunSpeed}m / {Mathf.Round(movement.RunSpeed / hexWidth)} hex");
+                __instance.dataList.Add("TT Walk MP", $"{movement.MovementPoint}");
             }
             catch (Exception e)
             {
                 Control.mod.Logger.LogError(e);
             }
-
-            mechDef = null;
-            movement = null;
-        }
-
-        public static float OverrideMaxWalkDistance(this MovementCapabilitiesDef @this)
-        {
-            try
-            {
-                movement = mechDef?.GetEngineMovement();
-                if (movement != null)
-                {
-                    return movement.WalkSpeed;
-                }
-            }
-            catch (Exception e)
-            {
-                Control.mod.Logger.LogError(e);
-            }
-            return @this.MaxWalkDistance;
-        }
-
-        public static float OverrideMaxSprintDistance(this MovementCapabilitiesDef @this)
-        {
-            try
-            {
-                movement = mechDef?.GetEngineMovement();
-                if (movement != null)
-                {
-                    return movement.RunSpeed;
-                }
-            }
-            catch (Exception e)
-            {
-                Control.mod.Logger.LogError(e);
-            }
-            return @this.MaxSprintDistance;
         }
     }
 }
