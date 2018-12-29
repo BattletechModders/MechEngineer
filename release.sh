dff70a8a403e8cc9adea4ae86392096dd01aeafb
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-12-29 13:26:01 +0100

    release script updated to exclude nuget packages and compress zip files

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ SEVENZIP="/c/Program Files/7-Zip/7z"
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 # -ir!HardpointFixMod
 INCLUDES_ALL="$INCLUDES -ir!BattletechPerformanceFix -ir!SkipIntro -ir!SkipTutorial -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod ModTek.dll"
-EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!.vs -xr!.git -xr!_ignored -xr!*.zip"
+EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git -xr!_ignored -xr!*.zip"
 
-"$SEVENZIP" a -tzip -mx0 MechEngineer/MechEngineer.zip $EXCLUDES $INCLUDES
-"$SEVENZIP" a -tzip -mx0 MechEngineer/MechEngineerWorkspace.zip $EXCLUDES $INCLUDES_ALL
+"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES $INCLUDES
+"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineerWorkspace.zip $EXCLUDES $INCLUDES_ALL
