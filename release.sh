653ab448dba67e6c41d01e99b22fa1e96efa1085
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-01-13 15:17:16 +0100

    Updated to ModTek 5.0.1 release.

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ SEVENZIP="/c/Program Files/7-Zip/7z"
 
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 # -ir!HardpointFixMod
-INCLUDES_ALL="$INCLUDES -ir!BattletechPerformanceFix -ir!SkipIntro -ir!SkipTutorial -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod ModTek.dll"
+INCLUDES_ALL="$INCLUDES -ir!AdjustedMechSalvage -ir!BattletechPerformanceFix -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod ModTek.dll"
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git -xr!_ignored -xr!*.zip"
 
 "$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES $INCLUDES
