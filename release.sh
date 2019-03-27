4d09ca2b18276b6173fa8f0e039f815514991e3d
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-03-27 10:08:16 +0100

    Reduced the release zip contents for ME.

--- release.sh
+++ release.sh
@@ -9,7 +9,9 @@ SEVENZIP="/c/Program Files/7-Zip/7z"
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 # -ir!HardpointFixMod
 INCLUDES_ALL="$INCLUDES -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek"
-EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!_ignored -xr!*.zip"
 
-"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES $INCLUDES
-"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineerWorkspace.zip $EXCLUDES $INCLUDES_ALL
+EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh"
+EXCLUDES_ALL="$EXCLUDES -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
+
+"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES_ALL $INCLUDES
+#"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineerWorkspace.zip $EXCLUDES $INCLUDES_ALL
