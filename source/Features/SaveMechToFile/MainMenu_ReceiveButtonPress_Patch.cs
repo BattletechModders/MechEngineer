7e2c83a2f07c003bc7be2ebb8a99e57e3d28f8f0
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-04-26 15:17:35 +0200

    Added event audio debugging, disabled.

---
+++ source/Features/SaveMechToFile/MainMenu_ReceiveButtonPress_Patch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BattleTech.UI;
+using Harmony;
+using HBS;
+using HBS.Scripting.Reflection;
+using TMPro;
+
+namespace MechEngineer.Features.SaveMechToFile
+{
+    //[HarmonyPatch(typeof(MainMenu), "ReceiveButtonPress")]
+    public static class MainMenu_ReceiveButtonPress_Patch
+    {
+        private static IEnumerator<string> Iterator = null;
+
+        public static bool Prefix(TextMeshProUGUI ____version, string button)
+        {
+            if (Iterator == null || (button == "Skirmish" && !Iterator.MoveNext()))
+            {
+                SceneSingletonBehavior<WwiseManager>.Instance.LoadCombatBanks();
+                SceneSingletonBehavior<WwiseManager>.Instance.LoadCombatBanks();
+                Iterator = EventIds();
+            }
+
+            var eventId = Iterator.Current;
+            ____version.text = eventId;
+            SceneSingletonBehavior<WwiseManager>.Instance.StopAllAudio();
+            WwiseManager.PostEvent(Iterator.Current, WwiseManager.GlobalAudioObject);
+            Control.mod.Logger.Log($"WwiseManager.PostEvent eventName={eventId}");
+
+            // AudioEventList_aircraft.aircraft_leopard_destruction
+            return false;
+        }
+
+        public static IEnumerator<string> EventIds()
+        {
+            Type eventListAttr = typeof(WwiseEventList);
+            var wwiseEnums = ReflectionUtil.TypesWithAttributeInAppDomain(typeof(WwiseManagedEnum)).ToList<Type>();
+            foreach (var eType in wwiseEnums)
+            {
+                var typeName = eType.Name;
+                if (!eType.IsDefined(eventListAttr, true))
+                {
+                    continue;
+                }
+                var values = Enum.GetValues(eType);
+                foreach (var eVal in values)
+                {
+                    var valueName = eVal.ToString();
+                    var guid = typeName + "_" + valueName;
+                    yield return guid;
+                }
+            }
+        }
+    }
+}
