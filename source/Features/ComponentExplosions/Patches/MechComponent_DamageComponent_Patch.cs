25278b2f3df2dad037eaa3b33c7bc94be36aa5c6
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-04-29 11:46:50 +0200

    Fixed unnecessary patch numbering.

--- source/Features/ComponentExplosions/Patches/MechComponent_DamageComponent_Patch.cs
+++ source/Features/ComponentExplosions/Patches/MechComponent_DamageComponent_Patch.cs
@@ -5,7 +5,7 @@ using Harmony;
 namespace MechEngineer.Features.ComponentExplosions.Patches
 {
     [HarmonyPatch(typeof(MechComponent), nameof(MechComponent.DamageComponent))]
-    public static class MechComponent_DamageComponent_Patch2
+    public static class MechComponent_DamageComponent_Patch
     {
         public static void Postfix(MechComponent __instance, WeaponHitInfo hitInfo, ComponentDamageLevel damageLevel, bool applyEffects)
         {
--- source/Features/Engines/Patches/Mech_InitEffectStats_Patch.cs
+++ source/Features/Engines/Patches/Mech_InitEffectStats_Patch.cs
@@ -6,7 +6,7 @@ using MechEngineer.Features.Engines.StaticHandler;
 namespace MechEngineer.Features.Engines.Patches
 {
     [HarmonyPatch(typeof(Mech), "InitEffectStats")]
-    public static class Mech_InitEffectStats2_Patch
+    public static class Mech_InitEffectStats_Patch
     {
         // change the movement stats when loading into a combat game the first time
         public static void Postfix(Mech __instance)
