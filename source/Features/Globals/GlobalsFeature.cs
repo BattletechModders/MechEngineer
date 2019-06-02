0197ae803aec29a50bcf62e091e1b62f712273f9
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-06-02 10:01:08 +0200

    Added dependencies to GlobalsFeature.

--- source/Features/Globals/GlobalsFeature.cs
+++ source/Features/Globals/GlobalsFeature.cs
@@ -1,7 +1,12 @@
-﻿namespace MechEngineer.Features.Globals
+﻿using MechEngineer.Features.Engines;
+using MechEngineer.Features.OverrideTonnage;
+
+namespace MechEngineer.Features.Globals
 {
     internal class GlobalsFeature : Feature
     {
         internal static readonly GlobalsFeature Shared = new GlobalsFeature();
+
+        internal override bool Enabled => OverrideTonnageFeature.Shared.Enabled || EngineFeature.Shared.Enabled;
     }
 }
