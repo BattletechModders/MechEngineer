4eba781513ee724c40c73137f62edf6929123b5d
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-06-08 15:35:02 +0200

    Improved ShutdownInjuryProtection feature description.

--- source/Features/ShutdownInjuryProtection/ShutdownInjuryProtectionSettings.cs
+++ source/Features/ShutdownInjuryProtection/ShutdownInjuryProtectionSettings.cs
@@ -3,7 +3,7 @@
     public class ShutdownInjuryProtectionSettings : ISettings
     {
         public bool Enabled { get; set; } = true;
-        public string EnabledDescription => "Pilots receive injury when mech receives overheat damage or has to shut down.";
+        public string EnabledDescription => "Pilots receive injury when mech receives overheat damage or has to shut down in case life support is destroyed.";
 
         public bool HeatDamageInjuryEnabled = true;
         public bool ShutdownInjuryEnabled = true;
