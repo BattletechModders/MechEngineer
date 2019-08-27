ff1067e7934f403b7d9bf1ba58ffa128230edc45
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-08-27 21:35:12 +0200

    Replace ARM MOUNTED with e.g. RA MOUNTED.

---
+++ source/Features/AccuracyEffects/Patches/CombatHUDWeaponSlot_AddToolTipDetail_Patch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BattleTech;
+using BattleTech.UI;
+using Harmony;
+
+namespace MechEngineer.Features.AccuracyEffects.Patches
+{
+    [HarmonyPatch(typeof(CombatHUDWeaponSlot), "AddToolTipDetail")]
+    public static class CombatHUDWeaponSlot_AddToolTipDetail_Patch
+    {
+        public static bool Prefix(Weapon ___displayedWeapon, ref string description)
+        {
+            try
+            {
+                if (description == "ARM MOUNTED")
+                {
+                    var location = Mech.GetAbbreviatedChassisLocation((ChassisLocations) ___displayedWeapon.Location);
+                    description = $"{location} MOUNTED";
+
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Control.mod.Logger.LogError(e);
+            }
+
+            return true;
+        }
+    }
+}
