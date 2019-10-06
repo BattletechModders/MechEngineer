d51e1afa9afd867b7a387c48e959a7bff13a09f7
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-10-06 18:13:45 +0200

    Disable auto-screenshots in skirmish mechlab by default.

--- source/Features/DebugScreenshotMechs/DebugScreenshotMechsSettings.cs
+++ source/Features/DebugScreenshotMechs/DebugScreenshotMechsSettings.cs
@@ -2,7 +2,7 @@
 {
     public class DebugScreenshotMechsSettings : ISettings
     {
-        public bool Enabled { get; set; } = true;
+        public bool Enabled { get; set; } = false;
         public string EnabledDescription => "Edit a mech then cancel, it then will go through all mechs and make screenshots for them. To improve loading times use the TagManager to blacklist all components so the MechLab doesn't have to load them.";
 
         public string ScreenshotDirectoryPath { get; set; } = "screenshots";
