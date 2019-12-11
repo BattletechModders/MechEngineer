8ec1172c29e47462c7f9c76c75e521f9559b8ee2
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-12-11 19:15:14 +0100

    Reformatted some classes to standard VS formatting.

--- source/Features/OverrideDescriptions/BonusDescriptionSettings.cs
+++ source/Features/OverrideDescriptions/BonusDescriptionSettings.cs
@@ -1,15 +1,17 @@
 ﻿using Localize;
 
-namespace MechEngineer.Features.OverrideDescriptions {
-  internal class BonusDescriptionSettings {
-    private string FShort;
-    private string FLong;
-    private string FFull;
+namespace MechEngineer.Features.OverrideDescriptions
+{
+    internal class BonusDescriptionSettings
+    {
+        private string FShort;
+        private string FLong;
+        private string FFull;
 #pragma warning disable 649
-    public string Bonus;
-    public string Short { get { return new Text(FShort).ToString();  } set { FShort = value; } }
-    public string Long { get { return new Text(FLong).ToString(); } set { FLong = value; } }
-    public string Full { get { return new Text(FFull).ToString(); } set { FFull = value; } }
+        public string Bonus;
+        public string Short { get { return new Text(FShort).ToString(); } set { FShort = value; } }
+        public string Long { get { return new Text(FLong).ToString(); } set { FLong = value; } }
+        public string Full { get { return new Text(FFull).ToString(); } set { FFull = value; } }
 #pragma warning restore 649
-  }
+    }
 }
