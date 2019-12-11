4aff1366ce7fbdf75c0baefc54f808d8a3187abd
CptMoore <39010654+CptMoore@users.noreply.github.com>
2019-12-11 19:56:03 +0100

    Ignore idea.

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ SEVENZIP="/c/Program Files/7-Zip/7z"
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 INCLUDES_ALL="$INCLUDES -ir!MechEngineerCheats -ir!BTDebug -ir!DebugToolsEnabler -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek -ir!CommanderPortraitLoader"
 
-EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh"
+EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea"
 EXCLUDES_ALL="$EXCLUDES -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
 "$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES_ALL $INCLUDES
