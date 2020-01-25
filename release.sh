e9a98f062f01e6fc76f13c37abddcc760eea823b
CptMoore <39010654+CptMoore@users.noreply.github.com>
2020-01-25 09:33:41 +0100

    Ignore HBS folder for release.

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 INCLUDES_ALL="$INCLUDES -ir!MechEngineerCheats -ir!BTDebug -ir!DebugToolsEnabler -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek -ir!CommanderPortraitLoader"
 
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea"
-EXCLUDES_ALL="$EXCLUDES -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
+EXCLUDES_ALL="$EXCLUDES -xr!HBS -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
 "$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES_ALL $INCLUDES
 "$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineerWorkspace.zip $EXCLUDES $INCLUDES_ALL
