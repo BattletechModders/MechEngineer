6f3ed7e6edddd8a82a31d6ef70c2fd443b007906
CptMoore <39010654+CptMoore@users.noreply.github.com>
2020-01-25 20:54:40 +0100

    Simplified and consoliated StatTooltipData patching.

--- source/Features/Engines/Patches/StatTooltipData_SetHeatData_Patch.cs
+++ source/Features/Engines/Patches/StatTooltipData_SetHeatData_Patch.cs
@@ -17,16 +17,12 @@ namespace MechEngineer.Features.Engines.Patches
                 var stats = new MechDefHeatEfficiencyStatistics(mechDef);
 
                 var tooltipData = __instance;
-                void Replace(string text, string value, string newText = null)
-                {
-                    tooltipData.dataList.Remove(text);
-                    tooltipData.dataList.Add(newText ?? text, value);
-                }
+                tooltipData.dataList.Clear();
 
-                Replace(Strings.T("Heat Sinking"), Strings.T("{0} Heat", stats.HeatSinking), "<u>" + Strings.T("Heat Sinking") + "</u>");
-                Replace(Strings.T("Alpha Strike"), Strings.T("{0} Heat", stats.AlphaStrike), "<u>" + Strings.T("Alpha Strike") + "</u>");
-                Replace(Strings.T("Avg. Jump Heat"), Strings.T("{0} Heat", stats.JumpHeat), Strings.T("Jump Heat"));
-                Replace(Strings.T("Shutdown"), Strings.T("{0} / {1} Heat", stats.Overheat, stats.MaxHeat), Strings.T("Heat Levels"));
+                tooltipData.dataList.Add("<u>" + Strings.T("Heat Sinking") + "</u>", Strings.T("{0} Heat", stats.HeatSinking));
+                tooltipData.dataList.Add("<u>" + Strings.T("Alpha Strike") + "</u>", Strings.T("{0} Heat", stats.AlphaStrike));
+                tooltipData.dataList.Add(Strings.T("Jump Heat"), Strings.T("{0} Heat", stats.JumpHeat));
+                tooltipData.dataList.Add(Strings.T("Heat Levels"), Strings.T("{0} / {1} Heat", stats.Overheat, stats.MaxHeat));
             }
             catch (Exception e)
             {
--- source/Features/Engines/Patches/StatTooltipData_SetMovementData_Patch.cs
+++ source/Features/Engines/Patches/StatTooltipData_SetMovementData_Patch.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using BattleTech;
+﻿using BattleTech;
 using Harmony;
 using Localize;
 using MechEngineer.Features.Engines.Helper;
 using MechEngineer.Features.OverrideTonnage;
-using UnityEngine;
+using System;
 
 namespace MechEngineer.Features.Engines.Patches
 {
@@ -19,19 +18,20 @@ namespace MechEngineer.Features.Engines.Patches
                 var stats = new MechDefMovementStatistics(mechDef);
 
                 var tooltipData = __instance;
-                void ReplaceDistance(string text, float meter, string newText = null)
+                tooltipData.dataList.Clear();
+
+                static string DistanceToSummary(float meter)
                 {
                     var meters = PrecisionUtils.RoundDownToInt(meter);
                     var hexWidth = MechStatisticsRules.Combat.MoveConstants.ExperimentalGridDistance;
                     var hexes = PrecisionUtils.RoundDownToInt(meters / hexWidth);
                     var translatedValue = Strings.T("{0}m / {1} hex", meters, hexes);
-                    tooltipData.dataList.Remove(text);
-                    tooltipData.dataList.Add(newText ?? text, translatedValue);
+                    return translatedValue;
                 }
 
-                ReplaceDistance(Strings.T("Max Move"), stats.WalkSpeed);
-                ReplaceDistance(Strings.T("Max Sprint"), stats.RunSpeed, "<u>" + Strings.T("Max Sprint") + "</u>");
-                ReplaceDistance(Strings.T("Max Jump"), stats.JumpDistance);
+                tooltipData.dataList.Add(Strings.T("Max Move"), DistanceToSummary(stats.WalkSpeed));
+                tooltipData.dataList.Add("<u>" + Strings.T("Max Sprint") + "</u>", DistanceToSummary(stats.RunSpeed));
+                tooltipData.dataList.Add(Strings.T("Max Jump"), DistanceToSummary(stats.JumpDistance));
                 tooltipData.dataList.Add(Strings.T("TT Walk MP"), $"{stats.WalkMovementPoint}");
             }
             catch (Exception e)
