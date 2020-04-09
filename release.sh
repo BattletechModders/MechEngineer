b0ccb47294625c5afec68ebd5642aa633e32240e
CptMoore <39010654+CptMoore@users.noreply.github.com>
2020-04-09 16:24:20 +0200

    Integrated CI using github workflows
    Added github workflow.

--- release.sh
+++ release.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+PATH="/c/Program Files/7-Zip/:$PATH"
+
+SEVENZIP="7z"
+
 set -ex
+
+MEZIP="MechEngineer/dist/MechEngineer.zip"
+MEALLZIP="MechEngineer/dist/MechEngineerWorkspace.zip"
+rm -f "$MEZIP" "$MEALLZIP"
 
 cd ..
 
-SEVENZIP="/c/Program Files/7-Zip/7z"
+(
+cd CustomComponents/source
+#git describe --exact-match
+dotnet build --configuration Release --no-incremental -p:OutputPath=../ "$@"
+)
 
+(
+cd MechEngineer/source
+#git describe --exact-match
+dotnet build --configuration Release --no-incremental -p:OutputPath=../ "$@"
+)
+
+(
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
-INCLUDES_ALL="$INCLUDES -ir!MechEngineerCheats -ir!BTDebug -ir!DebugToolsEnabler -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek -ir!CommanderPortraitLoader"
+INCLUDES_ALL="$INCLUDES -ir!MechEngineerCheats -ir!BTDebug -ir!DebugToolsEnabler -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek"
 
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea"
-EXCLUDES_ALL="$EXCLUDES -xr!HBS -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
+EXCLUDES_ALL="$EXCLUDES -xr!HBS -xr!CCTestMod -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
-"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineer.zip $EXCLUDES_ALL $INCLUDES
-"$SEVENZIP" a -tzip -mx9 MechEngineer/MechEngineerWorkspace.zip $EXCLUDES $INCLUDES_ALL
+"$SEVENZIP" a -tzip -mx9 "$MEZIP" $EXCLUDES_ALL $INCLUDES
+"$SEVENZIP" a -tzip -mx9 "$MEALLZIP" $EXCLUDES_ALL $INCLUDES_ALL
+)
