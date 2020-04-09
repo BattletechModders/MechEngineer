fec9e3e1d59951d916c97f8e28cba9610c99d705
CptMoore <39010654+CptMoore@users.noreply.github.com>
2020-04-09 13:21:32 +0200

    Next CI try.

--- release.sh
+++ release.sh
@@ -29,7 +29,7 @@ INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 INCLUDES_ALL="$INCLUDES -ir!MechEngineerCheats -ir!BTDebug -ir!DebugToolsEnabler -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek"
 
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea"
-EXCLUDES_ALL="$EXCLUDES -xr!HBS -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
+EXCLUDES_ALL="$EXCLUDES -xr!HBS -xr!CCTestMod -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
 "$SEVENZIP" a -tzip -mx9 "$MEZIP" $EXCLUDES_ALL $INCLUDES
 "$SEVENZIP" a -tzip -mx9 "$MEALLZIP" $EXCLUDES_ALL $INCLUDES_ALL
