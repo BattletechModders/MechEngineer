c8f2a4869201e55208b3f03bf97448425408e48a
CptMoore <39010654+CptMoore@users.noreply.github.com>
2020-08-29 15:51:12 +0200

    Added ECM refresh performance fix. Only update ECM cover at end of movement and not during.

---
+++ source/Features/Performance/Patches/AbstractActor_OnPositionUpdate_Patch - Copy.cs
@@ -0,0 +1,16 @@
+﻿using BattleTech;
+using Harmony;
+
+namespace MechEngineer.Features.Performance.Patches
+{
+    [HarmonyPatch(typeof(ActorMovementSequence), "CompleteMove")]
+    public static class ActorMovementSequence_CompleteMove_Patch
+    {
+        public static void Postfix(ActorMovementSequence __instance)
+        {
+            var traverse = Traverse.Create(__instance);
+            var combat = traverse.Property("Combat").GetValue<CombatGameState>();
+            AuraCache.RefreshECMStates(combat.AllActors, EffectTriggerType.Passive);
+        }
+    }
+}
---
+++ source/Features/Performance/Patches/AbstractActor_OnPositionUpdate_Patch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using BattleTech;
+using Harmony;
+using UnityEngine;
+
+namespace MechEngineer.Features.Performance.Patches
+{
+    [HarmonyPatch(typeof(AbstractActor), nameof(AbstractActor.OnPositionUpdate))]
+    public static class AbstractActor_OnPositionUpdate_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.MethodReplacer(
+                AccessTools.Method(typeof(AuraCache), nameof(AuraCache.UpdateAurasToActor)),
+                AccessTools.Method(typeof(AbstractActor_OnPositionUpdate_Patch), nameof(UpdateAurasToActor))
+            );
+        }
+
+        public static void UpdateAurasToActor(
+            List<AbstractActor> actors,
+            AbstractActor movingActor,
+            Vector3 movingActorPosition,
+            EffectTriggerType triggerSource,
+            bool forceUpdate)
+        {
+            // ignore position updates
+        }
+    }
+}
