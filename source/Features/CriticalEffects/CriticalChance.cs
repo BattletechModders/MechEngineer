ae23e79cf7adf658eb9af1c1c7c227764b845007
CptMoore <39010654+CptMoore@users.noreply.github.com>
2021-04-16 23:25:28 +0200

    Added CriticalChance custom to avoid having strong coupling to CriticalEffects.

---
+++ source/Features/CriticalEffects/CriticalChance.cs
@@ -0,0 +1,10 @@
+﻿using CustomComponents;
+
+namespace MechEngineer.Features.CriticalEffects
+{
+    [CustomComponent("CriticalChance")]
+    public class CriticalChance : SimpleCustomComponent
+    {
+        public int Size { get; set; } // allows the override the inventory size in a location
+    }
+}
