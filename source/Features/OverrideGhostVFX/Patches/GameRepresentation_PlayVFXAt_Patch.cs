ffad6d67745f1ffa68a8b87e6082266c7ae18950
CptMoore <39010654+CptMoore@users.noreply.github.com>
2021-06-04 18:18:26 +0200

    Stop complaining rider!

--- source/Features/OverrideGhostVFX/Patches/GameRepresentation_PlayVFXAt_Patch.cs
+++ source/Features/OverrideGhostVFX/Patches/GameRepresentation_PlayVFXAt_Patch.cs
@@ -5,7 +5,7 @@ using Harmony;
 
 namespace MechEngineer.Features.OverrideGhostVFX.Patches
 {
-    [HarmonyPatch(typeof(GameRepresentation), nameof(GameRepresentation.PlayVFXAt))]
+    [HarmonyPatch(typeof(GameRepresentation), "PlayVFXAt")]
     public static class GameRepresentation_PlayVFXAt_Patch
     {
         public static bool Prefix(string vfxName)
