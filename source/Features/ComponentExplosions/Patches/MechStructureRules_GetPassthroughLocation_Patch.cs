4170935785611e0c99b5299c46a81e43075707f9
CptMoore <39010654+CptMoore@users.noreply.github.com>
2021-10-27 20:30:29 +0200

    Added head to center torso passthrough.

---
+++ source/Features/ComponentExplosions/Patches/MechStructureRules_GetPassthroughLocation_Patch.cs
@@ -0,0 +1,19 @@
+﻿using BattleTech;
+using Harmony;
+
+namespace MechEngineer.Features.ComponentExplosions.Patches
+{
+    [HarmonyPatch(typeof(MechStructureRules), nameof(MechStructureRules.GetPassthroughLocation))]
+    internal static class MechStructureRules_GetPassthroughLocation_Patch
+    {
+        public static bool Prefix(ArmorLocation location, ref ArmorLocation __result)
+        {
+            if (location == ArmorLocation.Head)
+            {
+                __result = ArmorLocation.CenterTorso;
+                return false;
+            }
+            return true;
+        }
+    }
+}
