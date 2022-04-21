95c06d74db1b23e1cf32c8ef7881f1ab4e38c9f1
barak1001 <barak1001@hotmail.com>
2022-04-21 15:57:27 -0500

    Initial Upload

---
+++ source/Misc/StoredTonnage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleTech;
+using MechEngineer.Features.OverrideTonnage;
+
+namespace MechEngineer.Misc;
+
+public class StoredTonnage
+{
+    public static float UnRoundedTonnage;
+}
