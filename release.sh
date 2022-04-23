f3ad9b09c9f0d8ec28c3c286e8139ae13d60093c
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-04-23 13:10:50 +0200

    Release.sh cleanup.

--- release.sh
+++ release.sh
@@ -6,16 +6,17 @@ SEVENZIP="7z"
 
 set -ex
 
+(
+dotnet build -c Release "$@"
+)
+
+(
 cd ..
 
 MEZIP="MechEngineer/dist/MechEngineer.zip"
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.DotSettings -xr!*.sln -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
-(
-cd MechEngineer
-dotnet build -c Release "$@"
-)
-
 rm -f "$MEZIP"
 "$SEVENZIP" a -tzip -mx9 "$MEZIP" $EXCLUDES $INCLUDES
+)
