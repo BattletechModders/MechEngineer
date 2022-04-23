b1b4f8139bf5e7888505591231b556ff6dc6339d
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-04-23 12:07:13 +0200

    Simplified workflow.

--- release.sh
+++ release.sh
@@ -6,31 +6,16 @@ SEVENZIP="7z"
 
 set -ex
 
-MEZIP="MechEngineer/dist/MechEngineer.zip"
-MEALLZIP="MechEngineer/dist/MechEngineerWorkspace.zip"
-rm -f "$MEZIP" "$MEALLZIP"
-
 cd ..
 
-(
-cd CustomComponents/source
-#git describe --exact-match
-dotnet build --configuration Release --no-incremental -p:OutputPath=../ "$@"
-)
-
-(
-cd MechEngineer/source
-#git describe --exact-match
-dotnet build --configuration Release --no-incremental -p:OutputPath=../ "$@"
-)
-
-(
+MEZIP="MechEngineer/dist/MechEngineer.zip"
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
-INCLUDES_ALL="$INCLUDES -ir!MechEngineerCheats -ir!BTDebug -ir!DebugToolsEnabler -ir!BattletechPerformanceFix -ir!SkipTravelCutscenes -ir!SkipTutorialCleaner -ir!SkipIntro -ir!CBTHeat -ir!DynModLib -ir!pansar -ir!SpeedMod -ir!ModTek"
+EXCLUDES="-xr!log.txt -xr!*.log -xr!*.DotSettings -xr!*.sln -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
-EXCLUDES="-xr!log.txt -xr!*.log -xr!*.DotSettings -xr!*.sln -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!.modtek -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea -xr!publicized_assemblies"
-EXCLUDES_ALL="$EXCLUDES -xr!HBS -xr!CCTestMod -xr!CommanderPortraitLoader/source -xr!CustomComponents/source -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
-
-"$SEVENZIP" a -tzip -mx9 "$MEZIP" $EXCLUDES_ALL $INCLUDES
-"$SEVENZIP" a -tzip -mx9 "$MEALLZIP" $EXCLUDES_ALL $INCLUDES_ALL
+(
+cd MechEngineer
+dotnet build -c Release "$@"
 )
+
+rm -f "$MEZIP"
+"$SEVENZIP" a -tzip -mx9 "$MEZIP" $EXCLUDES $INCLUDES
