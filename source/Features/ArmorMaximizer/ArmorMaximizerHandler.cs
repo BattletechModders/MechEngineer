e71311a0213c459e9ee5b5400e590734609042bb
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-05-01 00:31:47 +0200

    Simplified HandleArmorUpdate

--- source/Features/ArmorMaximizer/ArmorMaximizerHandler.cs
+++ source/Features/ArmorMaximizer/ArmorMaximizerHandler.cs
@@ -23,6 +23,7 @@ internal static class ArmorMaximizerHandler
 
         var locationStates = state.Locations.Values.ToArray();
         var stepSize = ArmorStructureRatioFeature.ArmorPerStep;
+        var changes = false;
 
         while (state.Remaining >= stepSize)
         {
@@ -50,9 +51,15 @@ internal static class ArmorMaximizerHandler
 
                 locationState.Assigned += stepSize;
                 state.Remaining -= stepSize;
+                changes = true;
 
                 Control.Logger.Trace?.Log($"OnMaxArmor location={location.GetShortString()} locationState={locationState}");
             }
+        }
+
+        if (!changes)
+        {
+            return;
         }
 
         void SetArmor(ChassisLocations location)
@@ -83,100 +90,42 @@ internal static class ArmorMaximizerHandler
             SetArmor(location);
         }
 
+        {
+            // how to fetch the correct widget and change the color
+            // not sure how to detect clicks on, but maybe alt and click on + or - to lock/unlock?
+            var widget = mechLabPanel.GetLocationWidget(ChassisLocations.Head);
+            widget.armorBar?.valueTextColor?.SetUIColor(UIColor.Gold); // this doesnt do anything
+            widget.armorBar?.nameTextColor?.SetUIColor(UIColor.Blue); // this works
+        }
+
         infoWidget.RefreshInfo();
         mechLabPanel.FlagAsModified();
         mechLabPanel.ValidateLoadout(false);
     }
 
-    internal static void HandleArmorUpdate(MechLabLocationWidget widget, bool isRearArmor, float amount)
+    internal static void HandleArmorUpdate(MechLabLocationWidget widget, bool isRearArmor, float direction)
     {
-        var mechDef = widget.mechLab.activeMechDef;
-        var tonsPerPoint = ArmorUtils.TonPerPointWithFactor(mechDef);
-        var freeTonnage = Weights.CalculateFreeTonnage(mechDef);
-        freeTonnage = ArmorUtils.RoundUp(freeTonnage, 0.0005f);
-        var armorWeight = amount * tonsPerPoint;
-        var ratio = widget.loadout.Location == ChassisLocations.Head ? 3 : 2;
-        var enforcedArmor = widget.chassisLocationDef.InternalStructure;
-        enforcedArmor = ArmorUtils.RoundDown(enforcedArmor, ArmorStructureRatioFeature.ArmorPerStep);
-        enforcedArmor *= ratio;
-        var currentArmor = widget.currentArmor;
-        var currentRearArmor = widget.currentRearArmor;
-        var maxArmor = enforcedArmor - currentArmor - currentRearArmor;
         var shiftModifierPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        var altModifierPressed = Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt);
-        if (isRearArmor)
+        var precision = shiftModifierPressed ? 1 : ArmorStructureRatioFeature.ArmorPerStep;
+        var stepDirection = direction < 0 ? -1 : 1;
+        var current = isRearArmor ? widget.currentRearArmor : widget.currentArmor;
+
+        var updated = stepDirection > 0
+            ? PrecisionUtils.RoundUp(current + 1, precision)
+            : PrecisionUtils.RoundDown(current - 1, precision);
+
+        // TODO not yet working
+        // ratio enforcement
+        if (direction > 0)
         {
-            currentArmor = widget.currentRearArmor;
+            var max = ArmorStructureRatioFeature.GetMaximumArmorPoints(widget.chassisLocationDef);
+            max -= isRearArmor
+                ? PrecisionUtils.RoundUpToInt(widget.currentArmor)
+                : PrecisionUtils.RoundUpToInt(widget.currentRearArmor);
+            Mathf.Min(updated, max);
         }
-        if (shiftModifierPressed)
-        {
-           amount *= 5f;
-            var divisible = ArmorUtils.IsDivisible(currentArmor, amount);
-            if (!divisible)
-            {
-                if (amount > 0)
-                {
-                    if (freeTonnage < tonsPerPoint) return;
-                    var multWeight = armorWeight * amount;
-                    if(multWeight > freeTonnage)
-                    {
-                        var freePoints = freeTonnage / tonsPerPoint;
-                        freePoints = ArmorUtils.RoundDown(freePoints,1);
-                        if (freePoints > 0)
-                        {
-                            currentArmor += freePoints;
-                            if(freePoints > maxArmor)
-                            {
-                                currentArmor += maxArmor;
-                            }
-                            widget.SetArmor(isRearArmor, currentArmor, true);
-                            return;
-                        }
-                        return;
-                    }
-                    currentArmor = ArmorUtils.RoundUp(currentArmor, amount);
-                    widget.SetArmor(isRearArmor, currentArmor, true);
-                    return;
-                }
-                if (amount < 0)
-                {
-                    if (currentArmor <= 0) return;
-                    currentArmor = ArmorUtils.RoundDown(currentArmor, amount);
-                    if(currentArmor < 0)
-                    {
-                        currentArmor = 0;
-                    }
-                    widget.SetArmor(isRearArmor, currentArmor);
-                    return;
-                }
-            }
-        }
-        if (amount > 0)
-        {
-            if (freeTonnage < tonsPerPoint) return;
-            if (maxArmor <= 0) return;
-            var multWeight = armorWeight * amount;
-            if (multWeight > freeTonnage)
-            {
-                var freePoints = freeTonnage / tonsPerPoint;
-                freePoints = ArmorUtils.RoundDown(freePoints, 1);
-                if (freePoints > 0)
-                {
-                    currentArmor += freePoints;
-                    if (freePoints > maxArmor)
-                    {
-                        currentArmor += maxArmor;
-                    }
-                    widget.SetArmor(isRearArmor, currentArmor, true);
-                    return;
-                }
-                return;
-            }
-        }
-        if (amount < 0)
-        {
-            if (currentArmor <= 0) return;
-        }
-        widget.ModifyArmor(isRearArmor, amount, true);
+
+        Control.Logger.Trace?.Log($"HandleArmorUpdate stepDirection={stepDirection} current={current} precision={precision} updated={updated} isRearArmor={isRearArmor}");
+        widget.SetArmor(isRearArmor, updated);
     }
 }
