5695546bd16a1c812dc484890249b476e4bb81f6
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-10-28 23:30:09 +0200

    Added more memory analysis.

---
+++ source/Features/DebugScreenshotMechs/Patches/Mesh_Constructor_Patch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Harmony;
+using MechEngineer.Misc;
+using UnityEngine;
+
+namespace MechEngineer.Features.DebugScreenshotMechs.Patches;
+
+[HarmonyPatch(typeof(Mesh), MethodType.Constructor)]
+public static class Mesh_Constructor_Patch
+{
+    internal static readonly Dictionary<int, string> ids = new();
+    private static readonly string outputFile = Path.Combine(Control.Mod.Directory, "mesh.txt");
+    private static readonly Dictionary<string, int> set = new();
+
+    [UsedByHarmony]
+    public static bool Prepare()
+    {
+        return DebugScreenshotMechsFeature.Shared.Settings.DumpGameObjectCounts;
+    }
+
+    [HarmonyPostfix]
+    public static void Postfix(Mesh __instance)
+    {
+        var stacktrace = new System.Diagnostics.StackTrace(2, false);
+        var caller = "";
+        for (var i = 0; i < 10; i++)
+        {
+            if (i >= stacktrace.FrameCount)
+            {
+                break;
+            }
+            var frame = stacktrace.GetFrame(i);
+            var method = frame.GetMethod();
+            if (i >= 1)
+            {
+                caller += " -> ";
+            }
+            caller += method.DeclaringType?.FullName + "." + method.Name;
+        }
+        ids[__instance.GetInstanceID()] = caller;
+        if (set.TryGetValue(caller, out var counter))
+        {
+            ++counter;
+        }
+        else
+        {
+            counter = 1;
+        }
+
+        set[caller] = counter;
+        File.WriteAllLines(outputFile,
+            set
+                .OrderByDescending(kv => kv.Value)
+                .ThenBy(kv => kv.Key)
+                .Select(kv => $"{kv.Value}|{kv.Key}")
+        );
+    }
+}
--- source/Features/DebugScreenshotMechs/Patches/SceneMemoryStatsDumper.cs
+++ source/Features/DebugScreenshotMechs/Patches/SceneMemoryStatsDumper.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using BattleTech;
+using BattleTech.UI.TMProWrapper;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -9,15 +11,17 @@ namespace MechEngineer.Features.DebugScreenshotMechs.Patches;
 
 internal static class SceneMemoryStatsDumper
 {
-    private static Dictionary<string, int> initial;
     internal const string MemoryFilename = "gameobjects.txt";
+    private static readonly string OutputFile = Path.Combine(Control.Mod.Directory, MemoryFilename);
+
+    private static Dictionary<string, int>? initial;
 
     internal static void DumpUiStuff()
     {
         var dict = new Dictionary<string, int>();
         foreach (var obj in Resources.FindObjectsOfTypeAll<Object>())
         {
-            if (obj is Component)
+            if (obj is Component && obj is not LocalizableText)
             {
                 continue;
             }
@@ -25,11 +29,16 @@ internal static class SceneMemoryStatsDumper
             string key;
             if (obj is GameObject go)
             {
-                var parents = GatherParents(go)
-                    .Reverse()
-                    .Where(name => name.StartsWith("ui", StringComparison.OrdinalIgnoreCase))
-                    .Take(2);
-                key = string.Join(" -> ", parents);
+                key = GetParentBreadCrumb(go);
+            }
+            else if (obj is Mesh mesh)
+            {
+                key = obj.GetType().Name + "|" + (Mesh_Constructor_Patch.ids.TryGetValue(mesh.GetInstanceID(), out var caller) ? caller : "<unknown>");
+            }
+            else if (obj is Component component)
+            {
+                //  + "|" + obj.name
+                key = obj.GetType().Name + "|" + GetParentBreadCrumb(component.gameObject);
             }
             else
             {
@@ -51,23 +60,60 @@ internal static class SceneMemoryStatsDumper
             initial = dict;
             return;
         }
-        var outputFile = Path.Combine(Control.Mod.Directory, MemoryFilename);
-        using var writer = new StreamWriter(outputFile);
-        foreach (var kv in dict
-                     .Select(kv =>
-                     {
-                         if (initial.TryGetValue(kv.Key, out var counter) && counter != kv.Value)
-                         {
-                             return new KeyValuePair<string?, int>(kv.Key, kv.Value - counter);
-                         }
-                         return new(null, 0);
-                     })
-                     .Where(kv => kv.Key != null)
-                     .OrderByDescending(kv => kv.Value)
-                     .ThenBy(kv => kv.Key))
+        File.WriteAllLines(OutputFile,
+        UnityGameInstance.BattleTechGame.DataManager.GameObjectPool.gameObjectPool
+                .OrderByDescending(kv => kv.Value.Count)
+                .ThenBy(kv => kv.Key)
+                .Select(kv => $"{kv.Value.Count}|Pool {kv.Key} ")
+                .Concat(
+                    dict
+                        .Select(kv =>
+                        {
+                            if (initial.TryGetValue(kv.Key, out var counter) && counter != kv.Value)
+                            {
+                                return new KeyValuePair<string?, int>(kv.Key, kv.Value - counter);
+                            }
+                            return new(null, 0);
+                        })
+                        .Where(kv => kv.Key != null)
+                        .OrderByDescending(kv => kv.Value)
+                        .ThenBy(kv => kv.Key)
+                        .Select(kv => $"{kv.Value}|{kv.Key}"))
+        );
+    }
+
+    private static string GetParentBreadCrumb(GameObject go)
+    {
+        string? tmpKey = null;
+        void AddName(string name)
         {
-            writer.WriteLine($"{kv.Value}|{kv.Key}");
+            if (tmpKey == null)
+            {
+                tmpKey = name;
+            }
+            else
+            {
+                tmpKey += " -> " + name;
+            }
         }
+        var uiCount = 0;
+        foreach (var name in GatherParents(go).Reverse())
+        {
+            if (uiCount == 3)
+            {
+                break;
+            }
+            if (name.StartsWith("uix", StringComparison.OrdinalIgnoreCase))
+            {
+                uiCount++;
+                AddName(name);
+            }
+            else if (uiCount < 1)
+            {
+                AddName(name);
+            }
+        }
+        return tmpKey ?? "<empty>";
     }
 
     private static IEnumerable<string> GatherParents(GameObject go)
