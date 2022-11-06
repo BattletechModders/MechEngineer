9cffe8199d7a62f55543820c71d641d1e7ea9708
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-11-06 02:11:05 +0100

    Allow DBGSkirmishMechList to modify request resources.

--- source/Misc/Mods.cs
+++ source/Misc/Mods.cs
@@ -3,6 +3,6 @@
 public static class Mods
 {
     public const string AC = "io.mission.activatablecomponents";
-    public const string CU = "io.mission.customunits";
+    public const string CU = "io.mission.customunits"; // also includes DBGSkirmishMechList
     public const string CC = "io.github.denadan.CustomComponents";
 }
