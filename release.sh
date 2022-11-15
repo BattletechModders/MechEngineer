89fcf6fbcab5436170c1c99a7b89037200894ad3
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-11-15 01:50:17 +0100

    Switched to mod builder.

--- release.sh
+++ release.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# 7z is supported on Windows and Linux within GitHub
 export PATH="/c/Program Files/7-Zip/:$PATH"
-
 SEVENZIP="7z"
 
-set -ex
+set -uex
 
-(
-dotnet build -c Release "$@"
+( # Deps
+cd "$MODS_DIR"
+curl -L -o CustomComponents.zip https://github.com/BattletechModders/CustomComponents/releases/download/latest/CustomComponents.zip
+unzip CustomComponents.zip
 )
 
-(
-cd ..
+( # Build
+dotnet build -c Release "-p:BattleTechGameDir=$BATTLETECH_DIR"
+)
 
-MEZIP="MechEngineer/dist/MechEngineer.zip"
+( # Dist
+cd "$MODS_DIR"
+
+MEZIP="$DIST_DIR/MechEngineer.zip"
 INCLUDES="-ir!MechEngineer -ir!CustomComponents"
 EXCLUDES="-xr!log.txt -xr!*.log -xr!*.DotSettings -xr!*.sln -xr!*.suo -xr!*.pdb -xr!*.user -xr!bin -xr!obj -xr!packages -xr!.vs -xr!.git* -xr!_ignored -xr!*.zip -xr!*.sh -xr!.idea -xr!MechEngineer/source -xr!MechEngineer/engine_generator"
 
