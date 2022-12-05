390d3e8b2c3fbeb52c69d9b77151f8490e030100
CptMoore <39010654+CptMoore@users.noreply.github.com>
2022-12-05 21:16:50 +0100

    Renamed to Heat Delta.

--- source/Features/OverrideStatTooltips/Stats/HeatEfficiencyStat.cs
+++ source/Features/OverrideStatTooltips/Stats/HeatEfficiencyStat.cs
@@ -15,7 +15,7 @@ internal class HeatEfficiencyStat : IStatHandler
         tooltipData.dataList.Add(Strings.T("End of Turn"), Strings.T("{0} Heat", - stats.HeatSinking));
         tooltipData.dataList.Add(Strings.T("End of Movement"), Strings.T("{0} Heat", stats.EndMoveHeat));
         tooltipData.dataList.Add("<u>" + Strings.T("Alpha Strike") + "</u>", Strings.T("{0} Heat", stats.AlphaStrike));
-        tooltipData.dataList.Add(Strings.T("Alpha - Sinking"), Strings.T("{0} Heat", stats.AlphaStrike - stats.HeatSinking + stats.EndMoveHeat));
+        tooltipData.dataList.Add(Strings.T("Heat Delta"), Strings.T("{0} Heat", stats.AlphaStrike - stats.HeatSinking + stats.EndMoveHeat));
         tooltipData.dataList.Add(Strings.T("Jumping"), Strings.T("{0} Heat", stats.JumpHeat));
         tooltipData.dataList.Add(Strings.T("Heat Levels"), Strings.T("{0} / {1} Heat", stats.Overheat, stats.MaxHeat));
     }
